978d65ead9ffe7c57497ff769c0e756d6d9e1f3c
gwaks <20202160@sungshin.ac.kr>
2023-09-29 15:45:10 +0900

    output 관련... - name - contents 로 현재 한 페이지(배터리 관련) 크롤링 가능
    - 여러 페이지 계속 크롤링하는 기능
    - 중복 제거 빠르게 하기
    - DB 저장될 칼럼 별로 저장하기

--- src/main/java/com/_02server/Croller.java
+++ src/main/java/com/_02server/Croller.java
@@ -6,10 +6,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Croller {
     public static void main(String[] args) {
+        // CSV 파일 경로
+        String csvFilePath = "output.csv";
 
         ChromeOptions options = new ChromeOptions();
         //options.addArguments("--headless");
@@ -18,21 +24,47 @@ public class Croller {
 
         driver.get("https://prod.danawa.com/list/?cate=20322920&15main_20_03");
 
-        WebElement prodInfo = driver.findElement(By.xpath("//div[@class='prod_info'][contains(text(), '상세 스펙')]"));
-        WebElement productNameElement = prodInfo.findElement(By.cssSelector("div.prod_name[name='productName']"));
+        WebElement prodInfo = driver.findElement(By.xpath("//div[@class='main_prodlist main_prodlist_list']"));
+        for (WebElement e : prodInfo.findElements(By.cssSelector("div.prod_main_info"))) {
+            String name = e.findElement(By.cssSelector("p.prod_name a[name='productName']")).getText();
 
-        String productName = productNameElement.getText();
-        System.out.println("상품 이름: "+ productName);
+            String contents = e.findElement(By.cssSelector("div.spec_list")).getText();
+            contents = contents.replaceAll("\\n","");
 
-        WebElement prodSpecSet = prodInfo.findElement(By.xpath(".//div[@class='prod_spec_set']/div[@class='screen_out']"));
-
-        if (prodSpecSet.getText().contains("상세 스펙")) {
-            List<WebElement> specElements = prodSpecSet.findElements(By.xpath(".//*"));
-            for (WebElement element : specElements) {
-                if(element.getText().matches(".*시간.*|.*배터리.*|.*용량.*|.*출력.*"))
-                    System.out.println(element.getText());
-            }
+            saveDataToCSV(name,contents,csvFilePath);
+            System.out.println("상품 이름: " + name + "\n  " + contents);
+            System.out.println();
         }
+
+
+//        WebElement prodSpecSet = prodInfo.findElement(By.xpath(".//div[@class='prod_spec_set']/div[@class='screen_out']"));
+//
+//        if (prodSpecSet.getText().contains("상세 스펙")) {
+//            List<WebElement> specElements = prodSpecSet.findElements(By.xpath(".//*"));
+//            for (WebElement element : specElements) {
+//                if(element.getText().matches(".*시간.*|.*배터리.*|.*용량.*|.*출력.*"))
+//                    System.out.println(element.getText());
+//            }
+//        }
         driver.quit();
+    }
+
+    private static void saveDataToCSV(String name, String contents, String filePath) {
+        try (Writer writer = new FileWriter(filePath, true)) {
+            List<String[]> data = new ArrayList<>();
+
+
+            // 데이터 행 추가
+            String[] row = {name, contents};
+            data.add(row);
+
+            for (String[] rowData : data) {
+                writer.write(String.join(" ※ ", rowData));
+            }
+            writer.write("\n");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
