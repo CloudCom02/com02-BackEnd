7285ac1e5abba81457915276729d98fa9e00f363
SURI <20202160@sungshin.ac.kr>
2023-11-28 16:46:38 +0900

    클라우드 bucket 연결 준비 job,cronjob 예정..

--- src/main/java/com/_02server/BucketService.java
+++ src/main/java/com/_02server/BucketService.java
@@ -9,22 +9,22 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class BucketService {
-    public static void main(String[] args) throws IOException {
-        // 서비스 계정에 대한 인증 키 파일 경로
-        //String keyFilePath = "instant-droplet-406401-561aaf7cb06e.json";
-
-        // GCS 클라이언트 초기화
-        Storage storage = StorageOptions.newBuilder()
-                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(keyFilePath)))
-                .build()
-                .getService();
-
-        // 버킷의 객체 목록 가져오기
-        Bucket bucket = storage.get("your-bucket-name");
-        bucket.list().iterateAll().forEach(blob -> {
-            System.out.println("Object Name: " + blob.getName());
-            System.out.println("Size: " + blob.getSize());
-            // 추가적인 작업 수행
-        });
-    }
+//    public static void maindd(String[] args) throws IOException {
+//        // 서비스 계정에 대한 인증 키 파일 경로
+//        String keyFilePath = "instant-droplet-406401-561aaf7cb06e.json";
+//
+//        // GCS 클라이언트 초기화
+//        Storage storage = StorageOptions.newBuilder()
+//                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(keyFilePath)))
+//                .build()
+//                .getService();
+//
+//        // 버킷의 객체 목록 가져오기
+//        Bucket bucket = storage.get("your-bucket-name");
+//        bucket.list().iterateAll().forEach(blob -> {
+//            System.out.println("Object Name: " + blob.getName());
+//            System.out.println("Size: " + blob.getSize());
+//            // 추가적인 작업 수행
+//        });
+//    }
 }
